01bfa45180300b1b98c62ac584525dede66acaf4
Nick Murray <nickmurray0502@gmail.com>
2025-03-14 17:48:58 -0400

    Fix $PORT variable expansion in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/bash 
-echo "Checking Chrome binary at runtime..." 
-ls -l /usr/local/bin/chrome 
-exec gunicorn --bind 0.0.0.0:\$PORT app:app 
+#!/bin/bash
+echo "Checking Chrome binary at runtime..."
+ls -l /usr/local/bin/chrome
+echo "Starting Gunicorn on port $PORT..."
+exec gunicorn --bind 0.0.0.0:"$PORT" app:app
